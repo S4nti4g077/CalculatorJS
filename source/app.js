6a64cbcafb50c1ff6dff499c67a0487b0b70850f
Your Name <santiago.perez777@gmail.com>
2022-08-22 14:43:51 +0200

    added calculation

--- source/app.js
+++ source/app.js
@@ -6,11 +6,58 @@ const calculator = {
 };
 
 function inputDigit(digit) {
-  const { displayValue } = calculator;
-  calculator.displayValue = displayValue === "0" ? digit : displayValue + digit;
+  const { displayValue, waitingForSecondOperand } = calculator;
+
+  if (waitingForSecondOperand === true) {
+    calculator.displayValue = digit;
+    calculator.waitingForSecondOperand = false;
+  } else {
+    calculator.displayValue =
+      displayValue === "0" ? digit : displayValue + digit;
+  }
   console.log(calculator);
 }
 
+function inputDecimal(dot) {
+  if (!calculator.displayValue.includes(dot)) {
+    calculator.displayValue += dot;
+  }
+}
+
+function handleOperator(nextOperator) {
+  const { firstOperand, displayValue, operator } = calculator;
+  const inputValue = parseFloat(displayValue);
+  if (operator && calculator.waitingForSecondOperand) {
+    calculator.operator = nextOperator;
+    console.log(calculator);
+    return;
+  }
+  if (firstOperand === null && !isNaN(inputValue)) {
+    calculator.firstOperand = inputValue;
+  } else if (operator) {
+    const result = calculate(firstOperand, inputValue, operator);
+    calculator.displayValue = String(result);
+    calculator.firstOperand = result;
+  }
+  calculator.waitingForSecondOperand = true;
+  calculator.operator = nextOperator;
+  console.log(calculator);
+}
+
+// https://freshman.tech/calculator/
+
+function calculate(firstOperand, secondOperand, operator) {
+  if (operator === "+") {
+    return firstOperand + secondOperand;
+  } else if (operator === "-") {
+    return firstOperand - secondOperand;
+  } else if (operator === "*") {
+    return firstOperand * secondOperand;
+  } else if (operator === "/") {
+    return firstOperand / secondOperand;
+  }
+  return secondOperand;
+}
 function updateDisplay() {
   const display = document.querySelector("#screen-display");
   display.value = calculator.displayValue;
@@ -26,7 +73,8 @@ keys.addEventListener("click", (event) => {
   }
   //   show our clicked button - operator
   if (target.classList.contains("operator")) {
-    console.log("operator", target.value);
+    handleOperator(target.value);
+    updateDisplay();
     return;
   }
   //   if it is a decimal, print decimal
@@ -44,23 +92,3 @@ keys.addEventListener("click", (event) => {
   inputDigit(target.value);
   updateDisplay();
 });
-
-function inputDecimal(dot) {
-  if (!calculator.displayValue.includes(dot)) {
-    calculator.displayValue += dot;
-  }
-}
-
-function handleOperator(nextOperator) {
-  const { displayValue, firstOperand, operator } = calculator;
-  const inputValue = parseFloat(displayValue);
-  if (firstOperand === null && isNaN(inputValue)) {
-    calculator.firstOperand = inputValue;
-    console.log(calculator.firstOperand);
-  }
-  calculator.waitingForSecondOperand = true;
-  calculator.operator = nextOperator;
-  console.log(calculator);
-}
-
-// https://freshman.tech/calculator/
